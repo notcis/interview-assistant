5363ba4056e0434a8be59e38ba21139e98e5be23
notcis <notcis07@gmail.com>
2025-08-06 11:46:04 +0700

    feat: add Cloudinary integration for file upload and deletion

---
+++ utils/cloudinary.ts
@@ -0,0 +1,43 @@
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const upload_file = (
+  file: string,
+  folder: string
+): Promise<{ public_id: string; url: string }> => {
+  return new Promise((resolve, reject) => {
+    cloudinary.v2.uploader.upload(
+      file,
+      {
+        resource_type: "auto",
+        folder,
+      },
+      (error, result) => {
+        if (error || !result) {
+          return reject(error || new Error("Upload failed"));
+        }
+        resolve({
+          public_id: result.public_id,
+          url: result.secure_url,
+        });
+      }
+    );
+  });
+};
+
+const delete_file = async (public_id: string): Promise<boolean> => {
+  const res = await cloudinary.v2.uploader.destroy(public_id);
+
+  if (res?.result === "ok") {
+    return true;
+  }
+
+  return false;
+};
+
+export { upload_file, delete_file };
